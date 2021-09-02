d69f01844731ab552fdaaa6b0e2b3900556b0411
Arsalan Fallahpour <arsalanfallahpour@outlook.com>
2021-09-02 01:51:42 -0700

    Add Clear Nuger Package Script Option

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ProjectCenter
+![NuGet](https://img.shields.io/tokei/lines/github/arsalanfallahpour/ProjectCenter?color=darkgreen&label=total%20lines%20of%20source%20code%20ProjectCenter)
--- src/BaseScripts/Projects/PowershellCenter/nuget-package-manager/Publish-NugetPackage-Options.ps1
+++ src/BaseScripts/Projects/PowershellCenter/nuget-package-manager/Publish-NugetPackage-Options.ps1
@@ -6,26 +6,41 @@ Invoke-Expression "& '$global:powershellCenterPath\Import-Useful-Modules.ps1'";
 AutoImportModule -FileName "Publish-NugetPackage";
 AutoImportModule -FileName "FolderBrowser-Dialog";
 AutoImportModule -FileName "Folder";
-AutoImportModule -FileName "Folder";
+AutoImportModule -FileName "ClearNugetPackages";
 
-$option1 = "Publish all NugetPackage<.nupkg> and <SymbolPackage>.snupkg file of a project";
-$option2 = "Publish all NugetPackage<.nupkg> file of a project";
-$option3 = "Publish all <SymbolPackage>.snupkg file of a project";
-$option4 = "Publish a NugetPackage<.nupkg> and <SymbolPackage>.snupkg file";
-$option5 = "Publish a NugetPackage<.nupkg> file";
-$option6 = "Publish a <SymbolPackage>.snupkg file";
+$option1 = "Clear all package file of a project";
+$option2 = "Publish all NugetPackage<.nupkg> and <SymbolPackage>.snupkg file of a project";
+$option3 = "Publish all NugetPackage<.nupkg> and <SymbolPackage>.snupkg file of a project";
+$option4 = "Publish all NugetPackage<.nupkg> file of a project";
+$option5 = "Publish all <SymbolPackage>.snupkg file of a project";
+$option6 = "Publish a NugetPackage<.nupkg> and <SymbolPackage>.snupkg file";
+$option7 = "Publish a NugetPackage<.nupkg> file";
+$option8 = "Publish a <SymbolPackage>.snupkg file";
 $optionExit = "Exit Publish nuget services";
 do {
     $selectedProjectType = NumericOptionProvider -Message "Choose one of restore options:" -Options @(
         $option1,
-        $option2
-        $option3,
+        $option2,
+        $option3
         $option4,
         $option5,
         $option6,
+        $option7,
         $optionExit );
     switch ($selectedProjectType) {
         $option1{ 
+            $projectBasePath = ShowFolderBrowserDialogForProjects -IncludeInSubFolders;
+            ClearNugetPackages -Path $projectBasePath;
+        }
+        $option2{ 
+            $projectBasePath = ShowFolderBrowserDialogForProjects -IncludeInSubFolders;
+            Get-ChildItem -Path $projectBasePath -Filter "*.nupkg" -Recurse | ForEach-Object {
+                if (([string][System.IO.Path]::GetDirectoryName($_.FullName)).Contains('bin\Release')) {
+                    ClearNugetPackages -NugetPackageFilePath $_.FullName  -IncludeNugetPackageSymbol;
+                }
+            };
+        }
+        $option3{ 
             $projectBasePath = ShowFolderBrowserDialogForProjects -IncludeInSubFolders;
             Get-ChildItem -Path $projectBasePath -Filter "*.nupkg" -Recurse | ForEach-Object {
                 if (([string][System.IO.Path]::GetDirectoryName($_.FullName)).Contains('bin\Release')) {
@@ -33,7 +48,7 @@ do {
                 }
             };
         }
-        $option2 { 
+        $option4 { 
             $projectBasePath = ShowFolderBrowialogForProjects -IncludeInSubFolders;
             Get-ChildItem -Path $projectBasePath -Filter "*.snupkg" -Recurse | ForEach-Object {
                 if (([string][System.IO.Path]::GetDirectoryName($_.FullName)).Contains('bin\Release')) {
@@ -41,19 +56,19 @@ do {
                 }
             };
         }
-        $option3 { 
+        $option5 { 
             $nugetPackageFilePath = ShowOpenFileDialogForProjects -FileTypeTitle "NugetPackage" -Filter "nuget package file (*.nupkg)|*.nupkg";
             if (([string][System.IO.Path]::GetDirectoryName($_.FullName)).Contains('bin\Release')) {
                 PublishNugetPackage -NugetPackageFilePath  $nugetPackageFilePath -IncludeNugetPackageSymbol;
             }
         }
-        $option4 { 
+        $option6 { 
             $nugetPackageFilePath = ShowOpenFileDialogForProjects -FileTypeTitle "NugetPackage" -Filter "nuget package file (*.nupkg)|*.nupkg";
             if (([string][System.IO.Path]::GetDirectoryName($_.FullName)).Contains('bin\Release')) {
                 PublishNugetPackage -NugetPackageFilePath  $nugetPackageFilePath;
             }
         }
-        $option5 { 
+        $option7 { 
             $nugetPackageFilePath = ShowOpenFileDialogForProjects -FileTypeTitle "SymbolPackage" -Filter "nuget symbol package file (*.snupkg)|*.snupkg";
             if (([string][System.IO.Path]::GetDirectoryName($_.FullName)).Contains('bin\Release')) {
                 PublishNugetPackage -NugetPackageFilePath  $nugetPackageFilePath;
